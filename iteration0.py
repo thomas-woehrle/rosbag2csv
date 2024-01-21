62c84d1ed2e8bb200b1cfe22213bb860cf506fed
thomas-woehrle <127286604+thomas-woehrle@users.noreply.github.com>
2024-01-21 02:07:40 +0000

    Delete iteration0.py from git

--- iteration0.py
+++
@@ -1,9 +0,0 @@
-import rosbag
-
-bag = rosbag.Bag('test.bag')
-topics = list(bag.get_type_and_topic_info()[1].keys())
-
-for topic, msg, t in bag.read_messages(topics=topics):
-    print(msg)
-    
-bag.close()
