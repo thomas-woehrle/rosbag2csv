4d99de723dbde6611d32ab17862d16366ae6c319
thomas-woehrle <thomas.e.woehrle@gmail.com>
2024-01-21 02:06:19 +0000

    Add proper time objects

--- iteration1.py
+++ iteration1.py
@@ -6,13 +6,13 @@ def write_messages_to_csv(dict_of_topics_and_msgs):
 bag = rosbag.Bag('test.bag')
 topics = list(bag.get_type_and_topic_info()[1].keys())
 
-interval = 0 # = input_interval
-next_interval_time = 0 # bag.start_time + interval
+interval = 0.5 # should be inputted as argument to script
+next_interval_time = bag.get_start_time() + interval 
 
 most_recent_messages={} # empty dict
 
 for topic, msg, t in bag.read_messages(topics=topics):
-    if t <= next_interval_time:
+    if t.to_sec() <= next_interval_time:
         most_recent_messages[topic] = msg
     else:
         write_messages_to_csv(most_recent_messages)
